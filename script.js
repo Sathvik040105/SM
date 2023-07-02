a52c51187f5759ec32b8b0cd62a87d7bed2070e4
Nagasai <nagasai36912@gmail.com>
2023-07-02 09:07:05 +0530

    Added input boxes

---
+++ script.js
@@ -0,0 +1,8 @@
+
+let inputButton = document.getElementById("input-button");
+inputButton.addEventListener("click", run);
+
+function run() {
+    document.getElementById("encrypted").value = document.getElementById("input").value;
+    
+}
