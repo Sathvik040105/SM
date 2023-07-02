d44ddf179b57fadabafc31e07e6d68a52d9b9232
Sathvik040105 <sathvikm@iisc.ac.in>
2023-07-02 20:40:27 +0530

    modified:   SaiMan.html 	new file:   desktop.ini 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -2,7 +2,21 @@
 let inputButton = document.getElementById("input-button");
 inputButton.addEventListener("click", run);
 
+let restartButton = document.getElementById("restart-button");
+restartButton.addEventListener("click", restart);
+
 function run() {
     document.getElementById("encrypted").value = document.getElementById("input").value;
-    
+    //return key value as the sum of all ascii values of the input string
+    let key = 0;
+    for (let i = 0; i < document.getElementById("input").value.length; i++) {
+        key += document.getElementById("input").value.charCodeAt(i);
+    }
+    document.getElementById("key").value = key;
+}
+
+function restart() {
+    document.getElementById("input").value = "";
+    document.getElementById("encrypted").value = "";
+    document.getElementById("key").value = "";
 }
