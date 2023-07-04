1daeae8629fbbc6e28c780f37f0e9d50e7a0ff01
Sathvik040105 <sathvikm@iisc.ac.in>
2023-07-04 20:50:45 +0530

    new file:   github.jpeg 	modified:   index.html 	new file:   linkedin.jpg 	new file:   linkedin2.png 	modified:   script.js 	modified:   style.css

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ inputButton.addEventListener("click", run);
 let restartButton = document.getElementById("restart-button");
 restartButton.addEventListener("click", restart);
 
+
+
 function run() {
     document.getElementById("encrypted").value = document.getElementById("input").value;
     //return key value as the sum of all ascii values of the input string
@@ -38,3 +40,30 @@ function openPage(pageName,elmnt,color) {
   // Get the element with id="defaultOpen" and click on it
   document.getElementById("defaultOpen").click();
 
+let linkedinsathvButton = document.getElementById("linkedin-button");
+linkedinsathvButton.addEventListener("click", website);
+  
+let linkedinsaiButton = document.getElementById("linkedin-button1");
+linkedinsaiButton.addEventListener("click", website1);
+
+function website() {
+    window.open(URL="https://www.linkedin.com/in/sathvik-manthri-365984259/");
+}
+
+function website1() {
+    window.open(URL="https://www.linkedin.com/in/nagasai-jajapuram-1b8a73273/");
+}
+
+let gitsathvButton = document.getElementById("git-button");
+gitsathvButton.addEventListener("click", website2);
+  
+let gitsaiButton = document.getElementById("git-button1");
+gitsaiButton.addEventListener("click", website3);
+
+function website2() {
+    window.open(URL="https://github.com/Sathvik040105/");
+}
+
+function website3() {
+    window.open(URL="https://github.com/Nagasai561/");
+}
